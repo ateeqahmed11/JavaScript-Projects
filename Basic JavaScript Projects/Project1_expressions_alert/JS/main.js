e3124ee689fbbfaff1c6899bb0eae2ca6f79c683
ateeqahmed11 <ateeqahmed11@yahoo.com>
2024-04-07 02:41:31 +0600

    new assignemnt task

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,21 @@
 var A = "This is a string";
 document.write(A);
 window.alert(A)
+document.write("This is line 1\nThis is line 2");
+document.write("This is line 1" + "This is line 2"+"This is line 3");
+var Name = "THe Arezzinis", Dad="Jeremiah", Son="Joseph",Mom="Cristophar";
+document.write(Name);
+document.write(3 + 3) 
+var old = 23 // define age in a variable
+
+//window.alert method
+window.alert("Hello, World!");
+//document.write variables
+document.write("FOr the last time, hello, world!")
+//Create two varibales
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end of the string";
+//Displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+//an expression
+document.write( 25 * 10); 
