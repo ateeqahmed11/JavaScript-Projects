917115b2d5f9089627974d76e068d8bcc1e562a2
ateeqahmed11 <ateeqahmed11@yahoo.com>
2024-04-09 07:34:35 +0600

    One page webiste

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -1,27 +1,24 @@
-// Open the Modal
-function openModal() {
+
+    function openModal() {
     document.getElementById("myModal").style.display = "block";
 }
 
-// Close the Modal
-function closeModal() {
+    function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+    var slideIndex = 1;
+    showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
+    function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
+    function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+    function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
@@ -29,12 +26,12 @@ function showSlides(n) {
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+    slides[i].style.display = "none";
+}
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
